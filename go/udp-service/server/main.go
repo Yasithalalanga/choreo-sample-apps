4ad1b7638cf1215cbaaab8f70c81822952b83b30
Yasitha Gamage <yasithamaxs@gmail.com>
2024-01-08 15:33:51 +0530

    Updated Server Settings

--- go/udp-service/server/main.go
+++ go/udp-service/server/main.go
@@ -1,66 +1,93 @@
 /*
- * Copyright (c) 2023, WSO2 LLC. (https://www.wso2.com/) All Rights Reserved.
+ * Copyright (c) 2024, WSO2 LLC. (http://www.wso2.com). All Rights Reserved.
  *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * This software is the property of WSO2 LLC. and its suppliers, if any.
+ * Dissemination of any information or reproduction of any material contained
+ * herein is strictly forbidden, unless permitted by WSO2 in accordance with
+ * the WSO2 Commercial License available at http://wso2.com/licenses.
+ * For specific language governing the permissions and limitations under
+ * this license, please see the license as well as any agreement you’ve
+ * entered into with WSO2 governing the purchase of this software and any
+ * associated services.
  */
-
 package main
 
 import (
 	"fmt"
-	"log"
 	"net"
+	"os"
 )
 
-func main() {
-	serverPort := 5050
+const (
+	udpPort = "5050"
+	tcpPort = "5050"
+)
 
-	// Resolve server address
-	serverAddr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", serverPort))
-	if err != nil {
-		log.Fatalf("Error resolving UDP address: %v", err)
-	}
-
-	// Listen on UDP connection
-	conn, err := net.ListenUDP("udp", serverAddr)
-	if err != nil {
-		log.Fatalf("Error listening on UDP address: %v", err)
-	}
-
+func handleUDPConnection(conn *net.UDPConn) {
 	defer conn.Close()
-	log.Printf("UDP server listening on %s", serverAddr)
+	buffer := make([]byte, 1024)
+	for {
+		n, addr, err := conn.ReadFromUDP(buffer)
+		if err != nil {
+			fmt.Println("UDP error:", err)
+			return
+		}
+		fmt.Printf("Received UDP data from %s:%d: %s\n", addr.IP, addr.Port, string(buffer[:n]))
+	}
+}
+
+func handleTCPConnection(conn net.Conn) {
+	defer conn.Close()
+	buffer := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("TCP error:", err)
+			return
+		}
+		fmt.Printf("Received TCP data from %s: %s\n", conn.RemoteAddr(), string(buffer[:n]))
+	}
+}
+
+func main() {
+	// Start UDP server
+	udpAddr, err := net.ResolveUDPAddr("udp", ":"+udpPort)
+	if err != nil {
+		fmt.Println("UDP address resolution error:", err)
+		os.Exit(1)
+	}
+	udpConn, err := net.ListenUDP("udp", udpAddr)
+	if err != nil {
+		fmt.Println("UDP listen error:", err)
+		os.Exit(1)
+	}
+	defer udpConn.Close()
+
+	fmt.Printf("UDP server listening on port:%s\n", udpPort)
+
+	go handleUDPConnection(udpConn)
+
+	// Start TCP server
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ":"+tcpPort)
+	if err != nil {
+		fmt.Println("TCP address resolution error:", err)
+		os.Exit(1)
+	}
+	tcpListener, err := net.ListenTCP("tcp", tcpAddr)
+	if err != nil {
+		fmt.Println("TCP listen error:", err)
+		os.Exit(1)
+	}
+	defer tcpListener.Close()
+
+	fmt.Printf("TCP server listening on port:%s\n", tcpPort)
 
 	for {
-		// Read from UDP connection
-		buffer := make([]byte, 1024)
-		bytesRead, clientAddr, err := conn.ReadFromUDP(buffer)
+		conn, err := tcpListener.Accept()
 		if err != nil {
-			log.Printf("Error reading UDP message: %v", err)
+			fmt.Println("TCP accept error:", err)
 			continue
 		}
-
-		log.Printf("> Received message from %s: %s\n", clientAddr.String(), string(buffer[:bytesRead]))
-
-		/// Prepare and send a response to the client
-		message := "Hello " + string(buffer[:bytesRead])
-		response := []byte(message)
-		_, err = conn.WriteToUDP(response, clientAddr)
-		if err != nil {
-			log.Printf("Error sending response: %v", err)
-		}
-
-		log.Printf("< Sent message to %s: %s\n", clientAddr.String(), message)
+		go handleTCPConnection(conn)
 	}
 }
